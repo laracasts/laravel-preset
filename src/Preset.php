6b8a149438b0b71ae8c4c32ef70c2d615fafcb2a
Jeffrey Way <jeffrey@jeffrey-way.com>
2018-05-17 14:44:19 -0400

    Update styles

--- src/Preset.php
+++ src/Preset.php
@@ -14,6 +14,7 @@ class Preset extends LaravelPreset
         static::updatePackages();
         static::updateMix();
         static::updateScripts();
+        static::updateStyles();
     }
 
     public static function cleanSassDirectory()
@@ -39,5 +40,10 @@ class Preset extends LaravelPreset
     {
         copy(__DIR__.'/stubs/app.js', resource_path('assets/js/app.js'));
         copy(__DIR__.'/stubs/bootstrap.js', resource_path('assets/js/bootstrap.js'));
+    }
+
+    public static function updateStyles()
+    {
+        File::put(resource_path('assets/sass/app.sass'), '');
     }
 }
